2e5f60e217617e23bb53a586fafaf4ca5f39796a
nandinis898 <nandinis898@gmail.com>
2026-05-23 21:53:14 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,29 +1,27 @@
 import sys
 import os
 from zlib import compress
+from zlib import decompress
 
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!", file=sys.stderr)
-
-    
     command = sys.argv[1]
-    if command == "init":
-        os.mkdir(".git")
-        os.mkdir(".git/objects")
-        os.mkdir(".git/refs")
-        with open(".git/HEAD", "w") as f:
-            f.write("ref: refs/heads/main\n")
-            print("Initialized git directory")
-    case ["cat-file", "-p", blob_sha]:
-    with open(f".git/objects/{blob_sha[:2]}/{blob_sha[2:]}", "rb") as f:
-        blob = decompress(f.read())
-        print(blob.split(b"\x00")[1].decode(), end="")
-        
-    case:
-        raise RuntimeError(f"Unknown command #{command}")
-
+    
+    match command:
+        case "init":
+            os.mkdir(".git")
+            os.mkdir(".git/objects")
+            os.mkdir(".git/refs")
+            with open(".git/HEAD", "w") as f:
+                f.write("ref: refs/heads/main\n")
+                print("Initialized git directory")
+        case "cat-file":
+            blob_sha = sys.argv[3]
+            with open(f".git/objects/{blob_sha[:2]}/{blob_sha[2:]}", "rb") as f:
+                blob = decompress(f.read())
+                print(blob.split(b"\x00")[1].decode(), end="")
+        case _:
+            raise RuntimeError(f"Unknown command #{command}")
 
 if __name__ == "__main__":
     main()
