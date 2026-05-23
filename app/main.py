b4f7c4f4b0bbd504f8b69070e68f55d56eb8aaeb
nandinis898 <nandinis898@gmail.com>
2026-05-23 22:02:50 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,27 +1,50 @@
-import sys
+
+import hashlib
 import os
-from zlib import compress
-from zlib import decompress
+import pathlib
+import sys
+import zlib
 
 
 def main():
-    command = sys.argv[1]
-    
+    command = sys.argv[1:]
     match command:
-        case "init":
+        case ["init", *_]:
             os.mkdir(".git")
             os.mkdir(".git/objects")
             os.mkdir(".git/refs")
             with open(".git/HEAD", "w") as f:
                 f.write("ref: refs/heads/main\n")
-                print("Initialized git directory")
-        case "cat-file":
-            blob_sha = sys.argv[3]
-            with open(f".git/objects/{blob_sha[:2]}/{blob_sha[2:]}", "rb") as f:
-                blob = decompress(f.read())
-                print(blob.split(b"\x00")[1].decode(), end="")
+            print("Initialized git directory")
+        case ["cat-file", *args, object_id]:
+            cat_file(*args, object_id=object_id)
+        case ["hash-object", "-w", file_path]:
+            contents = pathlib.Path(file_path).read_bytes()
+            header = f"blob {len(contents)}".encode()
+            object_contents = header + b"\0" + contents
+            object_id = hashlib.sha1(object_contents).hexdigest()
+            print(object_id)
+            object_file = pathlib.Path(".git/objects") / object_id[:2] / object_id[2:]
+            object_file.parent.mkdir(parents=True, exist_ok=True)
+            object_file.write_bytes(zlib.compress(object_contents))
         case _:
-            raise RuntimeError(f"Unknown command #{command}")
+            raise RuntimeError(f"Unknown command #{command[0]}")
+
+
+def cat_file(*flags, object_id):
+    object_file = pathlib.Path(".git/objects") / object_id[:2] / object_id[2:]
+    contents = zlib.decompress(object_file.read_bytes())
+    size, content = contents.split(b"\0", maxsplit=1)
+    match flags:
+        case ["-p"]:
+            print(content.decode(), end="")
+        case ["-s"]:
+            _, sz = size.decode().rsplit(maxsplit=1)
+            print(sz)
+        case _:
+            raise RuntimeError(f"Unknown flags #{flags} for cat-file")
+
 
 if __name__ == "__main__":
     main()
+
